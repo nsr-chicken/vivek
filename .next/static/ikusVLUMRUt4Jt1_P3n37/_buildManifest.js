7afd260ecfb64c07f9093ed5c2ec0003a9419ca1
anvesh balaji <anvesh.b@globeshout.com>
2020-12-26 11:37:17 +0530

    chnages

--- .next/static/ikusVLUMRUt4Jt1_P3n37/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-986b9ac52257fbcb5ebd.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome-df22a3d7756eb684f9e5.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhome"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- next.config.js
+++ next.config.js
@@ -1,38 +1,16 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-
-const debug = process.env.NODE_ENV !== "production";
 const path = require('path')
+
 module.exports = {
-  exportPathMap: function (  defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }) {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
     return {
       '/': { page: '/home' },
     }
   },
-  //assetPrefix: '',
-  assetPrefix: !debug ? '/vivek/' : '',
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  },sassOptions: {
+
+  sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }/*,
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */
+  }
 }
