b99179e7ba44f8ede6ad1039dc79d19ef4865e2b
anvesh balaji <anvesh.b@globeshout.com>
2020-12-26 09:04:47 +0530

    .next add

---
+++ .next/static/webpack/pages/_app.30d13104917de0972f08.hot-update.js
@@ -0,0 +1,177 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./assets/fonts/Montserrat/Montserrat-Medium.ttf":
+false,
+
+/***/ "./assets/fonts/Poppins/Poppins-Bold.ttf":
+false,
+
+/***/ "./assets/fonts/Poppins/Poppins-Regular.ttf":
+false,
+
+/***/ "./assets/fonts/Poppins/Poppins-SemiBold.ttf":
+false,
+
+/***/ "./assets/scss/index.scss":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-brands-400.eot":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-brands-400.svg":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-brands-400.ttf":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-brands-400.woff":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-brands-400.woff2":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-regular-400.eot":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-regular-400.svg":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-regular-400.ttf":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-regular-400.woff":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-regular-400.woff2":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.eot":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.svg":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.ttf":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.woff":
+false,
+
+/***/ "./assets/scss/vendors/fontawesome-free-5.12.1-web/webfonts/fa-solid-900.woff2":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/next/dist/compiled/postcss-loader/cjs.js?!./node_modules/resolve-url-loader/index.js?!./node_modules/sass-loader/dist/cjs.js?!./assets/scss/index.scss":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/next/dist/compiled/postcss-loader/cjs.js?!./node_modules/resolve-url-loader/index.js?!./node_modules/sass-loader/dist/cjs.js?!./assets/scss/vendors/animate/animate.css":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/next/dist/compiled/postcss-loader/cjs.js?!./node_modules/resolve-url-loader/index.js?!./node_modules/sass-loader/dist/cjs.js?!./assets/scss/vendors/fontawesome-free-5.12.1-web/css/all.css":
+false,
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+false,
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+false,
+
+/***/ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":
+false,
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return MyApp; });
+/* harmony import */ var _babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/extends */ "./node_modules/@babel/runtime/helpers/esm/extends.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+var _jsxFileName = "E:\\projects\\next\\NRS (vivek)\\pages\\_app.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+// import '../assets/scss/index.scss'
+function MyApp(_ref) {
+  var Component = _ref.Component,
+      pageProps = _ref.pageProps;
+  return __jsx(Component, Object(_babel_runtime_helpers_esm_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({}, pageProps, {
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 12
+    }
+  }));
+}
+_c = MyApp;
+
+var _c;
+
+$RefreshReg$(_c, "MyApp");
+
+;
+    var _a, _b;
+    // Legacy CSS implementations will `eval` browser code in a Node.js context
+    // to extract CSS. For backwards compatibility, we need to check we're in a
+    // browser context before continuing.
+    if (typeof self !== 'undefined' &&
+        // AMP / No-JS mode does not inject these helpers:
+        '$RefreshHelpers$' in self) {
+        var currentExports = module.__proto__.exports;
+        var prevExports = (_b = (_a = module.hot.data) === null || _a === void 0 ? void 0 : _a.prevExports) !== null && _b !== void 0 ? _b : null;
+        // This cannot happen in MainTemplate because the exports mismatch between
+        // templating and execution.
+        self.$RefreshHelpers$.registerExportsForReactRefresh(currentExports, module.i);
+        // A module can be accepted automatically based on its exports, e.g. when
+        // it is a Refresh Boundary.
+        if (self.$RefreshHelpers$.isReactRefreshBoundary(currentExports)) {
+            // Save the previous exports on update so we can compare the boundary
+            // signatures.
+            module.hot.dispose(function (data) {
+                data.prevExports = currentExports;
+            });
+            // Unconditionally accept an update to this module, we'll check if it's
+            // still a Refresh Boundary later.
+            module.hot.accept();
+            // This field is set when the previous version of this module was a
+            // Refresh Boundary, letting us know we need to check for invalidation or
+            // enqueue an update.
+            if (prevExports !== null) {
+                // A boundary can become ineligible if its exports are incompatible
+                // with the previous exports.
+                //
+                // For example, if you add/remove/change exports, we'll want to
+                // re-execute the importing modules, and force those components to
+                // re-render. Similarly, if you convert a class component to a
+                // function, we want to invalidate the boundary.
+                if (self.$RefreshHelpers$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)) {
+                    module.hot.invalidate();
+                }
+                else {
+                    self.$RefreshHelpers$.scheduleUpdate();
+                }
+            }
+        }
+        else {
+            // Since we just executed the code for the module, it's possible that the
+            // new exports made it ineligible for being a boundary.
+            // We only care about the case when we were _previously_ a boundary,
+            // because we already accepted this update (accidental side effect).
+            var isNoLongerABoundary = prevExports !== null;
+            if (isNoLongerABoundary) {
+                module.hot.invalidate();
+            }
+        }
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcGFnZXMvX2FwcC5qcyJdLCJuYW1lcyI6WyJNeUFwcCIsIkNvbXBvbmVudCIsInBhZ2VQcm9wcyJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUFBO0FBRWUsU0FBU0EsS0FBVCxPQUF5QztBQUFBLE1BQXhCQyxTQUF3QixRQUF4QkEsU0FBd0I7QUFBQSxNQUFiQyxTQUFhLFFBQWJBLFNBQWE7QUFDcEQsU0FBTyxNQUFDLFNBQUQseUZBQWVBLFNBQWY7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQSxLQUFQO0FBQ0g7S0FGdUJGLEsiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svcGFnZXMvX2FwcC4zMGQxMzEwNDkxN2RlMDk3MmYwOC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0ICcuLi9hc3NldHMvc2Nzcy9pbmRleC5zY3NzJ1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gTXlBcHAoeyBDb21wb25lbnQsIHBhZ2VQcm9wcyB9KSB7XHJcbiAgICByZXR1cm4gPENvbXBvbmVudCB7Li4ucGFnZVByb3BzfSAvPlxyXG59Il0sInNvdXJjZVJvb3QiOiIifQ==
