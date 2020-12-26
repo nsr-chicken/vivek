de926c3f006818f5ecc34503abbcc26b81f2de13
anvesh balaji <anvesh.b@globeshout.com>
2020-12-26 12:19:29 +0530

    init app

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const debug = process.env.NODE_ENV !== "production";
 module.exports = {
   exportPathMap: async function (
     defaultPathMap,
@@ -9,21 +8,6 @@ module.exports = {
       '/': { page: '/home' },
     }
   },
- //assetPrefix: '',
- assetPrefix: !debug ? '/vivek/' : 'NRS (vivek)',
- webpack: (config, { dev }) => {
-  // Perform customizations to webpack config
-  // console.log('webpack');
-  // console.log(config.module.rules, dev);
-  config.module.rules = config.module.rules.map(rule => {
-    if(rule.loader === 'babel-loader') {
-      rule.options.cacheDirectory = false
-    }
-    return rule
-  })
-  // Important: return the modified config
-  return config
-},
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   }
